b265d1c06ef11e5c22db0e1f99e5d69616915ab8
giovannettisamuele <96236513+giovannettisamuele@users.noreply.github.com>
2023-03-26 20:57:40 +0200

    Create threadnani2.java

---
+++ 2022-11-14_tpsit_laboratorio/laboratorio10/threadnani2.java
@@ -0,0 +1,20 @@
+class contatoreNani2 extends Thread {
+    public contatoreNani2(String nome){
+        super();
+        setName(nome);
+    }
+    public void run(){
+     for(int i = 0; i < 7; i++){
+     System.out.printl((i+1)+" "+getNam());    
+        }  
+    }
+}
+
+class ProvaNani2{
+    public static void main (String args[]){
+        Thread thr1 = new ContatoreNani2("tuzia");
+        Thread thr2 = new ContatoreNani2("tumadre");
+        thr1.start();
+        thr2.start();
+    }
+}
