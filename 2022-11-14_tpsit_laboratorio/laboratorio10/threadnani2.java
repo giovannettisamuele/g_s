9bfbd51ab31af65750695d0b8ad9e794f3a22dcc
giovannettisamuele <96236513+giovannettisamuele@users.noreply.github.com>
2023-03-27 08:39:17 +0200

    Update threadnani2.java

--- 2022-11-14_tpsit_laboratorio/laboratorio10/threadnani2.java
+++ 2022-11-14_tpsit_laboratorio/laboratorio10/threadnani2.java
@@ -12,8 +12,8 @@ class contatoreNani2 extends Thread {
 
 class ProvaNani2{
     public static void main (String args[]){
-        Thread thr1 = new ContatoreNani2("tuzia");
-        Thread thr2 = new ContatoreNani2("tumadre");
+        Thread thr1 = new ContatoreNani2("Capitan America");
+        Thread thr2 = new ContatoreNani2("Iron Man");
         thr1.start();
         thr2.start();
     }
